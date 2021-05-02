66db140054b03a6f47e183d093cbadde1cbe5f22
Neto Lemes <neto.lemes@ROIT.LOCAL>
2021-05-02 18:45:39 -0300

    feat(my-circle): my firts web component

---
+++ component.js
@@ -0,0 +1,39 @@
+class MyCircle extends HTMLElement {
+    constructor () {
+        super ()
+        const shadow = this.attachShadow({ mode: 'open'})
+        const raiz = document.createElement('span')
+        raiz.setAttribute('class', 'raiz')
+
+        const text = document.createElement('span')
+        text.setAttribute('class', 'text')
+
+        text.textContent = this.getAttribute('text')
+
+        raiz.appendChild(text)
+
+        const style = document.createElement('style')
+        style.textContent = `
+        .raiz {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 200px;
+            height: 200px;
+            border-radius: 100px;
+            background-color: ${this.getAttribute("cor") || "red" }
+        }
+
+        .text {
+            font-size: 2rem;
+            color: white;
+        }
+        `
+
+        shadow.appendChild(style)
+        shadow.appendChild(raiz)
+
+    }
+}
+
+customElements.define('my-circle', MyCircle)
